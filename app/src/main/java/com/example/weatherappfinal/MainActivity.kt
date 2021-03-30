fa62917e7eb6bebc650a77dfbf18fb61164b1d9c
Martynas Miliauskas <milixmar@gmail.com>
2021-03-30 18:11:01 +0200

    no changes to initially created layout, switched to a lattitude and longitude approach to call weather API. Lat, long, and api values not able to be called yet.

--- app/src/main/java/com/example/weatherappfinal/MainActivity.kt
+++ app/src/main/java/com/example/weatherappfinal/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.weatherappfinal
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.location.LocationManager
 import android.os.AsyncTask
 import android.os.Bundle
 import android.view.View
@@ -12,21 +16,32 @@ import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
 
-    val CITY: String = "delhi,in"
-    val API: String = "8118ed6ee68db2debfaaa5a44c832918" // Use your own API key
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        weatherTask().execute()
+        val locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        @SuppressLint("MissingPermission")
+        val location: Location? = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+        val LONG = location!!.longitude
+        val LATI = location!!.latitude
+        val API: String = "d4be7d07c44a9730e4af9f4a925e10c0"
 
+        weatherTask()
+        
     }
 
-    inner class weatherTask() : AsyncTask<String, Void, String>() {
-        override fun onPreExecute() {
+
+
+
+
+inner class weatherTask() : AsyncTask<String, Void, String>() {
+
+            override fun onPreExecute() {
             super.onPreExecute()
             /* Showing the ProgressBar, Making the main design GONE */
             findViewById<ProgressBar>(R.id.loader).visibility = View.VISIBLE
@@ -34,10 +49,13 @@ class MainActivity : AppCompatActivity() {
             findViewById<TextView>(R.id.errorText).visibility = View.GONE
         }
 
-        override fun doInBackground(vararg params: String?): String? {
-            var response:String?
+           override fun doInBackground(vararg params: String?): String? {
+
+
+                var response:String?
             try{
-                response = URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API").readText(
+
+                response = URL("api.openweathermap.org/data/2.5/weather?lat=${LATI}&lon=${LONG}&units=metric&appid=${API}").readText(
                         Charsets.UTF_8
                 )
             }catch (e: Exception){
@@ -57,7 +75,7 @@ class MainActivity : AppCompatActivity() {
                 val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
 
                 val updatedAt:Long = jsonObj.getLong("dt")
-                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt*1000))
+                val updatedAtText = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(updatedAt * 1000))
                 val temp = main.getString("temp")+"°C"
                 val tempMin = "Min Temp: " + main.getString("temp_min")+"°C"
                 val tempMax = "Max Temp: " + main.getString("temp_max")+"°C"
@@ -78,8 +96,8 @@ class MainActivity : AppCompatActivity() {
                 findViewById<TextView>(R.id.temp).text = temp
                 findViewById<TextView>(R.id.temp_min).text = tempMin
                 findViewById<TextView>(R.id.temp_max).text = tempMax
-                findViewById<TextView>(R.id.sunrise).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise*1000))
-                findViewById<TextView>(R.id.sunset).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset*1000))
+                findViewById<TextView>(R.id.sunrise).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunrise * 1000))
+                findViewById<TextView>(R.id.sunset).text = SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(Date(sunset * 1000))
                 findViewById<TextView>(R.id.wind).text = windSpeed
                 findViewById<TextView>(R.id.pressure).text = pressure
                 findViewById<TextView>(R.id.humidity).text = humidity
